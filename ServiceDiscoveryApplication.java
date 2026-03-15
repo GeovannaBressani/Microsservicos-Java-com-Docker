daa3b31d664835de45ee9cc3ac755357feaa6ee2
GeovannaBressani <142333928+GeovannaBressani@users.noreply.github.com>
2026-03-14 23:05:25 -0300

    ServiceDiscoveryApplication.java

--- ServiceDiscoveryApplication.java
+++ ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.microsservicos.servicediscovery;
+package com.microsservicos.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
